c1c1afdaba41f4913eb4ad8d122fe9f4eaec45c5
Hicka <hello@hicka.dev>
2021-08-23 22:03:11 +0200

    feat(api): add Database utility class

--- api/src/db.ts
+++ api/src/db.ts
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose';
+import mongoose, { FilterQuery } from 'mongoose';
 import { IQuestion, QuestionModel } from './models/Question';
 
 /**
@@ -10,15 +10,30 @@ class Database {
 	 * @param uri The connection URI to the database.
 	 */
 	async connect(uri: string): Promise<void> {
-		await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+		return void await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 	}
 
 	/**
 	 * Fetches questions form the database.
+	 * @param options 
 	 */
-	async fetchQuestions(options?: FetchQuestionsOptions): Promise<IQuestion[]> {
-		//TODO later lol
+	async fetchQuestions(options?: FetchQuestionsOptions): Promise<void> {
+		const filter: FilterQuery<IQuestion> =
+			(options && options.labels)
+				? { label: { $all: options.labels } } // Only entries with all the specified labels will be returned.
+				: {};
+		const query = QuestionModel.find(filter);
+		if (options && options.limit)
+			query.limit(Math.max(1, options.limit)); // Makes sure a decent limit is specified.
+		const docs = await query.exec();
+		// This gets rid of MongoDB properties.
+		return docs.map(({ answers, label, statement }) => ({ answers, label, statement }));
 	}
+
+	async addQuestion(question: IQuestion): Promise<void> {
+		return void QuestionModel.create(question);
+	}
+
 }
 
 /**
@@ -26,10 +41,14 @@ class Database {
  */
 interface FetchQuestionsOptions {
 	/**
-	 * The labels to filter
+	 * The labels to search for. Only questions that have these labels will be returned.
 	 */
 	labels?: string[]
+	/**
+	 * The maximum amount of questions to return.
+	 */
 	limit?: number;
 }
 
-export database;
+// We need only one database connection.
+export default new Database();
