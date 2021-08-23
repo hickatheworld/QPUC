3d92b56eee2be60203a6bc07d3ce0b67dab66da4
Hicka <hello@hicka.dev>
2021-08-23 09:54:02 +0200

    feat(api): add Question mongoose model

---
+++ api/src/models/Question.ts
@@ -0,0 +1,30 @@
+import { Schema, model } from 'mongoose';
+
+/**
+ * A question in QPUC.
+ */
+interface Question {
+	/**
+	 * The possible answers to this question. The first one of the array is the correct answer.
+	 */
+	answers: string[];
+	/**
+	 * The statement of this question.
+	 */
+	statement: string;
+	/**
+	 * The type of this question:
+	 * 0 - Duo: The player is given 2 possible answers and must choose one of them.
+	 * 1 - Square: The player is given 4 possible answers and must choose one of them.
+	 * 2 - Cash: The player is given no possible answer.
+	 */
+	type: Number;
+}
+
+const Question = new Schema<Question>({
+	answers: [String],
+	statement: String,
+	type: Number
+});
+
+export default model('question', Question);
