9afa8026ade159eb955b700f2212f637ea8e79d2
Hicka <hello@hicka.dev>
2021-08-24 22:26:50 +0200

    feat(api): add /questions/get REST endpoint

--- api/src/index.ts
+++ api/src/index.ts
@@ -4,6 +4,27 @@ require('dotenv').config();
 
 const server = express();
 
+/**
+ * /questions/get - Returns a list of questions
+ * Query parameters:
+ *  - (optional) labels: Labels to filter the questions, separated with commas.
+ *  - (optional) limit: The maximum amount of questions to return. Defaults to 50.
+ * Returns:
+ * An array of IQuestion.
+ */
+server.get('/questions/get', async (req: Request, res: Response) => {
+	let labels: string[];
+	let limit: number;
+	// We can use these type assertions as these values will always be string.
+	if (req.query.labels)
+		labels = (<string>req.query.labels).split(',');
+	if (req.query.limit)
+		// If the given parameter was not a valid integer, limit must remain undefined.
+		limit = parseInt(<string>req.query.limit) || undefined;
+	const questions = await db.fetchQuestions({ labels, limit });
+	res.send(questions);
+});
+
 db.connect(process.env.MONGO_URI).then(() => {
 	server.listen(process.env.PORT, () => console.log(`API Server listening on :${process.env.PORT}`));
 });
