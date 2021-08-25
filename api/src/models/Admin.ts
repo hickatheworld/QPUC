6bc438554a57275d7ed41b9762e0d06387dc46ab
Hicka <hello@hicka.dev>
2021-08-25 09:56:54 +0200

    feat(api): add admin model

---
+++ api/src/models/Admin.ts
@@ -0,0 +1,28 @@
+import { Schema, model } from 'mongoose';
+
+/**
+ * A QPUC Admin.
+ */
+export interface IAdmin {
+	/**
+	 * The hashed password of the admin.
+	 */
+	password: string;
+	/**
+	 * The username of the admin.
+	 */
+	username: string;
+}
+
+const AdminSchema = new Schema<IAdmin>({
+	password: {
+		required: true,
+		type: String
+	},
+	username: {
+		required: true,
+		type: String
+	}
+});
+
+export const AdminModel = model('admin', AdminSchema);
