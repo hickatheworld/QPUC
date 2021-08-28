e7f05a68c8b0b7c885090ef830c6667d0f8bdd90
Hicka <hello@hicka.dev>
2021-08-28 10:47:46 +0200

    feat(api): add /questions/add endpoint

--- api/src/routes/questions.ts
+++ api/src/routes/questions.ts
@@ -1,5 +1,7 @@
 import { Request, Response, Router } from 'express';
 import db from '../db';
+import { IQuestion } from '../models/Question';
+
 // Type assertion because TypeScript somehow doesn't want me to instanciate a Router.
 const router = new (<any>Router)();
 
@@ -22,6 +24,43 @@ router.get('/get', async (req: Request, res: Response) => {
 		limit = parseInt(<string>req.query.limit) || undefined;
 	const questions = await db.questions.fetch({ labels, limit });
 	res.send(questions);
+});
+
+/**
+ * /questions/add - Lets an admin add a question to the database.
+ * Body: IQuestion.
+ */
+router.put('/add', async (req: Request, res: Response) => {
+	if (!Array.isArray(req.body.answers))
+		return res.send({ sucsess: false, error: '\'answers\' must be a correct array.' });
+	if (typeof req.body.statement !== 'string')
+		return res.send({ sucsess: false, error: '\'statement\' must be a correct string.' });
+	if (!(req.body.type >= 0 && req.body.type <= 2) || !Number.isInteger(req.body.type))
+		// If req.body.type is not a number, the conditions will also return false so we don't need a type check before these comparisons.
+		return res.send({ sucsess: false, error: '\'type\' must be a correct integer in [0;2].' });
+	if (req.body.type === 0 && req.body.answers.length !== 2)
+		return res.send({ sucsess: false, error: '\'answers\' must be of length 2 if \'type\' is 0.' });
+	if (req.body.type === 1 && req.body.answers.length !== 4)
+		return res.send({ sucsess: false, error: '\'answers\' must be of length 4 if \'type\' is 1.' });
+	if (req.body.type === 2 && req.body.answers.length !== 1)
+		return res.send({ sucsess: false, error: '\'answers\' must be of length 1 if \'type\' is 2.' });
+	let warn = false;
+	const question: IQuestion = {
+		answers: req.body.answers,
+		statement: req.body.statement,
+		type: req.body.type
+	};
+	if (req.body.labels) {
+		if (Array.isArray(req.body.labels))
+			question.labels = req.body.labels;
+		else warn = true;
+	}
+	try {
+		await db.questions.add(question);
+		res.send(warn ? { success: true, warning: '\'labels\' is not a correct array. The questions has been added to the database with no label.' } : { success: true });
+	} catch (err) {
+		res.send({ success: false, error: `Internal Error: ${err.toString()}` });
+	}
 });
 
 export default router;
