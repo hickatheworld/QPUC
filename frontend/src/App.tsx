7019d3118a6f8d234b567f7814bc228f94b4874c
Hicka <hello@hicka.dev>
2021-09-02 09:36:58 +0200

    cleanup(frontend): clean initial content

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import io, { Socket } from 'socket.io-client';
+import React from 'react';
 
 function App() {
-	const [socket, setSocket] = useState<Socket>();
-	console.log('hey');
-	useEffect(() => {
-		const s = io('http://localhost:3030');
-		console.log(s);
-		setSocket(s);
-		s.on('rick-client', () => alert('Never gonna let you down.'));
-	}, [setSocket]);
-	const sendRick = () => {
-		if (socket)
-			socket.emit('rick-server');
-	}
 	return (
 		<div className='App'>
-			Never gonna give you up, never gonna let you down.
-			<button onClick={sendRick}>Rick</button>
 		</div>
 	);
 }
