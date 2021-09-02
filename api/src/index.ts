0c5946ebc58dafc763ad237f489a947e9e0987f5
Hicka <hello@hicka.dev>
2021-09-02 12:51:16 +0200

    feat(api): add / endpoint

--- api/src/index.ts
+++ api/src/index.ts
@@ -13,6 +13,13 @@ server.use(authMiddle);
 server.use('/admins', adminsRoutes);
 server.use('/questions', questionsRoutes);
 
+/**
+ * / - Endpoint just to check if given credentials are valid.
+ */
+server.get('/', (req: Request, res: Response) => {
+	res.send({ success: true });
+});
+
 db.connect(process.env.MONGO_URI).then(() => {
 	server.listen(process.env.PORT, () => console.log(`API Server listening on :${process.env.PORT}`));
 });
