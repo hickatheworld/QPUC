7917f0a4ff22a24d0232cacdbdedc6340d854654
Hicka <hello@hicka.dev>
2021-09-09 14:41:28 +0200

    feat(frontend): add `color` prop to LoadingIndicator

--- frontend/src/components/common/LoadingIndicator.tsx
+++ frontend/src/components/common/LoadingIndicator.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import '../../style/LoadingIndicator.css';
 function LoadingIndicator(props: LoadingIndicatorProps): React.ReactElement {
 	const s = props.size || 8;
-	const css = { width: `${s}px`, height: `${s}px`, margin: `0px ${s / 4}px` };
+	const css = { width: `${s}px`, height: `${s}px`, margin: `0px ${s / 4}px`, background: props.color || '#ffffff' };
 	return (
 		<div className='loading-indicator'>
 			<div className='loading-indicator-dot first' style={css}></div>
@@ -14,6 +14,10 @@ function LoadingIndicator(props: LoadingIndicatorProps): React.ReactElement {
 export default LoadingIndicator;
 
 interface LoadingIndicatorProps {
+	/**
+	 * The color of the loading indicator.
+	 */
+	color?: string;
 	/**
 	 * The size of each this loading indicator dot.
 	 */
